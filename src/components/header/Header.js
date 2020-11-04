f0afe9fc97a9e40294671bf8a1bec50bf8cbf0bc
Akhil Biju <akhil.biju2@mindtree.com>
2020-11-04 22:03:35 +0530

    Added logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,12 @@
 import './Header.scss';
+import Logo from '../../images/logo.png';
 
 function Header() {
   return (
     <header>
-      <div className="logo">logo</div>
+      <div className="logo">
+        <img src={Logo} alt="logo not available"></img>
+      </div>
       <div className="navlinks">links</div>
       <div className="controls">controls</div>
     </header>
