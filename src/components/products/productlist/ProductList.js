4e369528274b7b3656734b8b0e27a65d375c3390
Akhil Biju <akhil.biju2@mindtree.com>
2020-11-06 01:07:08 +0530

    Added card styles

--- src/components/products/productlist/ProductList.js
+++ src/components/products/productlist/ProductList.js
@@ -3,12 +3,27 @@ import LazyImage from '../LazyImage/LazyImage';
 import './ProductList.scss';
 
 function ProductList() {
+  const getDiscount = (item) => {
+    const price = item.price;
+    const max_price = item.compare_at_price;
+    const percent = ((max_price - price) / max_price) * 100;
+    return `(${Math.floor(percent)}% OFF)`;
+  };
   return (
     <div className="productlist">
       {productlist.map((item) => {
         return (
           <div key={item.id} className="card">
             <LazyImage src={item.image_src[0]} alt="not available" />
+            <div className="footer">
+              <div className="name">{item.vendor}</div>
+              <div className="description">{item.name}</div>
+              <div className="price">
+                <span className="listprice">${item.price}</span>
+                <span className="maxprice">${item.compare_at_price}</span>
+                <span className="discount">{getDiscount(item)}</span>
+              </div>
+            </div>
           </div>
         );
       })}
